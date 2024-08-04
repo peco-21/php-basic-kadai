8f10cd0d929a642bb00a7aa1fbbe8fafc96edddc
peco-21 <peco.21.mhe@gmail.com>
2024-08-05 06:17:54 +0900

    ファイル追加
    php7章課題

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>7章課題</title>
+</head>
+
+<body>
+  <?php
+  // 連想配列を作成
+  $vegetable = array(
+    "name" => "onion",
+    "price" => 200,
+    "weight" => 160
+  );
+
+  // 連想配列をブラウザに出力
+  print_r($vegetable);
+  ?>
+</body>
+
+</html>
